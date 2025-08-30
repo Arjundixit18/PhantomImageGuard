d5776f29483b949538908697949fa2f7409f57e3
Arjun Dixit <119109856+Arjundixit18@users.noreply.github.com>
2025-08-30 15:32:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.querySelectorAll('.image-container img').forEach(img => {
+  img.onerror = () => {
+    const parent = img.parentElement;
+    parent.classList.add('fallback');
+    img.remove();
+  };
+});
 
